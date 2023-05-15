19c743be71ece2e2afcc44d7b1d5e70bc2ba4fe5
Samer <samerdl@gmail.com>
2023-05-15 21:25:52 +0300

    Add files via upload

---
+++ React_Project-1/src/components/MainPage/NavBar.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import "./Nav.css";
+
+const NavBar = () => {
+  return (
+    <div className="Nav">
+      <Link to="/AboutPage" className="NavBarFont">
+        About Us
+      </Link>
+      <Link to="/" className="NavBarFont">
+        Home
+      </Link>
+      <div>
+        <Link to="/LoginPage" className="NavBarFont">
+          {" "}
+          Sign up /{" "}
+        </Link>
+        <Link to="/ExistingMemberLogin" className="NavBarFont">
+          {" "}
+          Log in
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default NavBar;
+
+// Nav Bar directing and re-directing the 4 main pages.
